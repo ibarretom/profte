0c55b1ec334799601ee12c79c2a67cd066eacd1e
Igor Barreto <igorbm.23@gmail.com>
2020-10-11 11:29:32 -0300

    Adicinei o slickpara o carrossel e um pouco de responsividade

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,5 +1,43 @@
-$('banner').slick({
-    infinite: true,
+$('.banner').slick({
+    dots: true,
     slidesToShow: 1,
-    sildesToScrow: 2.
-})
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+ });
+
+ $('.sapatos').slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 630,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            centerMode: true,
+          }
+        }
+      ]
+ });
