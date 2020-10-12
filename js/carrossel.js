0bc2a52c1dbb1ac7a9de0e6b87a0db849101fc7c
Igor Barreto <igorbm.23@gmail.com>
2020-10-12 15:55:43 -0300

    Updates

--- js/carrossel.js
+++ js/carrossel.js
@@ -4,6 +4,34 @@ $('.banner').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      }
+    ]
  });
 
  $('.sapatos').slick({
@@ -20,6 +48,7 @@ $('.banner').slick({
             slidesToScroll: 3,
             infinite: true,
             dots: true,
+            arrows: false,
           },
         },
         {
@@ -35,8 +64,7 @@ $('.banner').slick({
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
-            arrows: false,
-            centerMode: true,
+            arrows: true,
           }
         }
       ]
