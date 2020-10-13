05db691bc8ef446c1d5fa0bffe4abe1510d7ad25
Igor Barreto <igorbm.23@gmail.com>
2020-10-12 23:59:59 -0300

    Updates

--- js/carrossel.js
+++ js/carrossel.js
@@ -44,8 +44,8 @@ $('.banner').slick({
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 2,
             infinite: true,
             dots: true,
             arrows: false,
